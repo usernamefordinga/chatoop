ca36fab142719414f8efd97f20f08bce181271bb
usernamefordinga <142829351+usernamefordinga@users.noreply.github.com>
2024-01-31 13:34:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,21 +7,22 @@ const MessageSchema = require("./MessageDB")
 
 
 App.post("/api/v1/account/create", async (req, res) => {
-  const username = await req.body.username
-  const password = await req.body.password
+  console.log(req.body)
+  // const username = await req.body.username
+  // const password = await req.body.password
 
-  const User = {
-    "user_name": username,
-    "password": password
-  }
+  // const User = {
+  //   "user_name": username,
+  //   "password": password
+  // }
 
-  if (await UserSchema.findOne(User)) {
-    res.send("User already exists")
+  // if (await UserSchema.findOne(User)) {
+  //   res.send("User already exists")
     
-  } else {
-    res.send("User created")
-    await UserSchema.insertOne(User);
-  }
+  // } else {
+  //   res.send("User created")
+  //   await UserSchema.insertOne(User);
+  // }
 
   
   
