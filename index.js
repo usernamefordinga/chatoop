747c1d1376f331a148bc313a998bbe920b6c7f8b
usernamefordinga <142829351+usernamefordinga@users.noreply.github.com>
2024-01-31 09:13:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-{
-  "name": "nodejs",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "keywords": [],
-  "author": "",
-  "license": "ISC",
-  "dependencies": {
-    "@types/node": "^18.0.6",
-    "node-fetch": "^3.2.6"
+const Express = rqeuire("expres");
+const App = Express();
+const Port = 3000
+
+const UserSchema = require("./UserDB")
+const MessageSchema = require("./MessageDB")
+
+
+App.post("/api/v1/account/create", async (req, res) => {
+  const username = await req.body.username
+  const password = await req.body.password
+
+  const User = {
+    "user_name": username,
+    "password": password
   }
-}
+
+  if (await UserSchema.findOne(User)) {
+    res.send("User already exists")
+    
+  } else {
+    res.send("User created")
+    await UserSchema.insertOne(User);
+  }
+
+  
+  
+  
+})
+
+
+
+
+App.listen(Port, () => {
+  console.log(`Server is running at port ${Port}`);
+});
